58177de0b31e7f5c0f4ad8fe673dc1a64cce4ed2
Insu Byeon <yuki@yuki.kr>
2020-08-23 22:50:32 +0900

    🛠 리팩토링

--- SteviaLayoutExample/ViewController.swift
+++ SteviaLayoutExample/ViewController.swift
@@ -50,38 +50,14 @@ class ViewController: UIViewController {
         return node
     }()
 
-    
+    // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
         
-        self.view.subviews {
-            scrollView.subviews {
-                stackView
-            }
-        }
-        
-        [view1, view2, view3, view4]
-            .forEach { stackView.addArrangedSubview($0) }
-        
-        
-        self.scrollView.fillContainer()
-//        self.scrollView.Bottom == self.view.Bottom
-//        self.scrollView.Left == self.view.Left
-//        self.scrollView.Right == self.view.Right
-//        self.scrollView.Top == self.view.Top
-        
-        self.stackView.fillContainer()
-        equal(heights: self.stackView, self.scrollView)
-//        self.stackView.Left == self.scrollView.Left
-//        self.stackView.Right == self.scrollView.Right
-//        self.stackView.Top == self.scrollView.Top
-//        self.stackView.Bottom == self.scrollView.Bottom
-//        self.stackView.Height == self.scrollView.Height
-        
-        equal(sizes: self.view1, self.scrollView)
-
+        self.setupSubviews()
+        self.setupAutoLayout()
         
     }
 
@@ -90,10 +66,36 @@ class ViewController: UIViewController {
 // MARK: - Private Methods
 extension ViewController {
     private func setupSubviews() {
+        self.view.subviews {
+            scrollView.subviews {
+                stackView
+            }
+        }
         
+        [view1, view2, view3, view4]
+            .forEach { stackView.addArrangedSubview($0) }
     }
     
     private func setupAutoLayout() {
+        // 아래의 주석과 동일함
+        self.scrollView.fillContainer()
+//        self.scrollView.Bottom == self.view.Bottom
+//        self.scrollView.Left == self.view.Left
+//        self.scrollView.Right == self.view.Right
+//        self.scrollView.Top == self.view.Top
         
+        
+        // 아래의 주석과 동일함
+        self.stackView.fillContainer()
+        equal(heights: self.stackView, self.scrollView)
+        
+//        self.stackView.Left == self.scrollView.Left
+//        self.stackView.Right == self.scrollView.Right
+//        self.stackView.Top == self.scrollView.Top
+//        self.stackView.Bottom == self.scrollView.Bottom
+//        self.stackView.Height == self.scrollView.Height
+        
+        // view1 만 사이즈 조절해두면 UIStackView가 알아서 나머지 뷰도 조절!
+        equal(sizes: self.view1, self.scrollView)
     }
 }
